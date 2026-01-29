127d662e5668025d11273eb5f06c06bf462626d4
OhSieun <sieun125@tukorea.ac.kr>
2026-01-29 20:44:20 +0900

    플레이어 구분을 ip:port가 아니라 서버에서 권위적으로 부여하는 플레이어 고유 아이디로 변경. 자신의 캐릭터와 상대 캐릭터 구분 가능.

--- OnlineGame_client/Player.h
+++ OnlineGame_client/Player.h
@@ -18,12 +18,19 @@ struct Player {
 	RGB color;
 };
 
-map<string, Player> otherPlayers; //key = "ip:port"
+int myPlayerId = -1;
+Player player{ Vec2{0.0f, 0.0f}, 0.5f, 0.5f, RGB{0.0f, 0.0f, 1.0f} };
+map<string, Player> otherPlayers; //key = "playerId"
 std::mutex playersMutex;
+
+void Set_MyPlayerID(int id)
+{
+	myPlayerId = id;
+}
 
 void HandleStateMessage(const string& msg) 
 {
-	//형식: STATE|ip:port|x|y|width|height|r|g|b
+	//형식: STATE|playerId|x|y|width|height|r|g|b
 
 	vector<string> tokens;
 	string temp;
@@ -38,25 +45,32 @@ void HandleStateMessage(const string& msg)
 	}
 	tokens.push_back(temp);
 
-	if(tokens.size()!=4)
+	if(tokens.size()<4)
 		return;
 	if (tokens[0] != "STATE")
 		return;
 
-	string id = tokens[1];
+	int id = atoi(tokens[1].c_str());
 	float x = (float)atof(tokens[2].c_str());
 	float y = (float)atof(tokens[3].c_str());
+	
+
+	if (id == myPlayerId){
+		std::lock_guard<std::mutex> lock(playersMutex);
+		player.pos = Vec2{ x, y }; //내 플레이어 위치 업데이트
+		return; //내 플레이어는 무시
+	}
 
 	std::lock_guard<std::mutex> lock(playersMutex);
 
-	auto it = otherPlayers.find(id);
+	auto it = otherPlayers.find(tokens[1]);
 	if (it == otherPlayers.end()) {
 		//새 플레이어 추가
-		otherPlayers[id] = Player{ Vec2{x, y}, 0.5f, 0.5f, RGB{0.0f, 0.0f, 1.0f} };
+		cout << "New player joined: " << id << " at (" << x << ", " << y << ")\n";
+		otherPlayers[tokens[1]] = Player{ Vec2{x, y}, 0.5f, 0.5f, RGB{0.0f, 0.0f, 1.0f} };
 	}
 	else {
 		//기존 플레이어 위치 업데이트
-		it->second.pos.x = x;
-		it->second.pos.y = y;
+		it->second.pos = Vec2{ x, y };
 	}
 }
